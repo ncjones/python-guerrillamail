cebc2fa7df076ab7270784b24f8112f94cac1971
Nathan Jones <nathan@ncjones.com>
2014-02-20 22:52:34 +1300

    Fix setup installation dependencies.

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ except ImportError:
 
 setup(
     name='python-guerrillamail',
-    version='0.1.0',
+    version='0.1.1',
     description='Client for the Guerrillamail temporary email server',
     author='Nathan Jones',
     url='https://github.com/ncjones/python-guerrillamail',
     py_modules=['guerrillamail'],
-    requires=[
-        'requests(==2.2.1)',
+    install_requires=[
+        'requests',
     ],
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
