b4830b023d626d1e485d74602319d4fecea3d73d
Nathan Jones <nathan@ncjones.com>
2016-06-12 17:54:54 +1200

    add release history document

---
+++ HISTORY.rst
@@ -0,0 +1,26 @@
+Change History
+--------------
+
+0.2.0
++++++
+
+ + Add support for Python 3.3, 3.4 and 3.5.
+ + Add "guerrillamail" executable.
+
+
+0.1.2
++++++
+
+ + Fix mail "excerpt" property.
+
+
+0.1.1
++++++
+
+ + Remove version restriction on Requests dependency.
+
+
+0.1.0
++++++
+
+ + Initial release.
--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ from codecs import open
 
 with open('README.rst', 'r', 'utf-8') as f:
     readme = f.read()
+with open('HISTORY.rst', 'r', 'utf-8') as f:
+    history = f.read()
 
 setup(
     name='python-guerrillamail',
     version='0.2.0',
     description='Client for the Guerrillamail temporary email server',
-    long_description=readme,
+    long_description=readme + '\n\n' + history,
     keywords='guerrillamail email client cli',
     author='Nathan Jones',
     url='https://github.com/ncjones/python-guerrillamail',
