1bc4efde36c234b818b4d149f16a3b8e22937ba0
Sam Tozer <samtozer96@gmail.com>
2020-08-05 22:41:52 +0100

    Remove long description builder which caused the package to error out when trying to pip install.

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 from setuptools import setup
-from codecs import open
 
-
-with open('README.rst', 'r', 'utf-8') as f:
-    readme = f.read()
-with open('HISTORY.rst', 'r', 'utf-8') as f:
-    history = f.read()
 
 setup(
     name='python-guerrillamail',
     version='0.2.0',
     description='Client for the Guerrillamail temporary email server',
-    long_description=readme + '\n\n' + history,
     keywords='guerrillamail email client cli',
     author='Nathan Jones',
     url='https://github.com/ncjones/python-guerrillamail',
